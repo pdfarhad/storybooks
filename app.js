31ed8f05b0e5b5587a7499151a00e815cef1c6c4
farhad_naeem <pdcsedu@gmail.com>
2019-03-04 13:29:21 +0600

    google auth

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const auth = require('./routes/auth')
+const passport = require('passport')
+require('./config/passport')(passport)
+
+
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -8,6 +13,10 @@ app.get('/', (req, res) => {
     res.send("It works")
 })
 
+app.use('/auth', auth)
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
+
+app.post
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb://root:farhad123@ds131258.mlab.com:31258/story-book",
+    googleClientID: "275763317628-mm7du8n3lotbkturbevah0t0nr1h7si9.apps.googleusercontent.com",
+    googleClientSecret: "J-5PhpsJkIFBclq8lT4Tmiy1"
+}
